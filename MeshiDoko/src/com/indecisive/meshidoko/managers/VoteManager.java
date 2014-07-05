c1fd3bd0fc89c95e5ed79b10aef05d531f7fa8eb
Junichi Kobayashi <nnujabok@gmail.com>
2014-07-05 18:17:55 +0900

    Temp

--- MeshiDoko/src/com/indecisive/meshidoko/managers/VoteManager.java
+++ MeshiDoko/src/com/indecisive/meshidoko/managers/VoteManager.java
@@ -1,15 +1,21 @@
 package com.indecisive.meshidoko.managers;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class VoteManager implements Serializable{
+import com.indecisive.meshidoko.models.Restaurant;
+
+public class VoteManager implements Serializable {
 	private static final long serialVersionUID = -4734049795992977745L;
 	private int[] voteTotals = new int[3];
 	private int voteNum;
 	private int voteCount = 0;
+	private List<Restaurant> restaurants = new ArrayList<Restaurant>();
 
 	public VoteManager(int peopleNum) {
 		voteNum = peopleNum;
+		restaurants.add(new Restaurant());
 	}
 
 	// restaurantId is 0 ~ 2 for now because there are only 3 restaurants
@@ -17,7 +23,7 @@ public class VoteManager implements Serializable{
 		voteTotals[restaurantId]++;
 		voteCount++;
 	}
-	
+
 	public int getVoteTotalOfRestaurant(int restaurantId) {
 		return voteTotals[restaurantId];
 	}
@@ -28,5 +34,25 @@ public class VoteManager implements Serializable{
 		} else {
 			return true;
 		}
+	}
+
+	public List<Restaurant> getRestaurants() {
+		return restaurants;
+	}
+
+	public void setRestaurants(List<Restaurant> restaurants) {
+		this.restaurants = restaurants;
+	}
+
+	public Restaurant getVoteResult() {
+		Restaurant result = restaurants.get(0);
+		int max = 0;
+		for (int i = 0; i < 3; i++) {
+			if (voteTotals[i] > voteTotals[max]) {
+				result = restaurants.get(i);
+				max = i;
+			}
+		}
+		return result;
 	}
 }
--- MeshiDoko/src/com/indecisive/meshidoko/result/ResultActivity.java
+++ MeshiDoko/src/com/indecisive/meshidoko/result/ResultActivity.java
@@ -1,10 +1,19 @@
 package com.indecisive.meshidoko.result;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import com.indecisive.meshidoko.R;
 import com.indecisive.meshidoko.managers.VoteManager;
+import com.indecisive.meshidoko.models.Restaurant;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultActivity extends Activity {
 	private VoteManager voteManager;
@@ -13,6 +22,26 @@ public class ResultActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result);
-		voteManager = (VoteManager)getIntent().getExtras().getSerializable("voteManager");
+		voteManager = (VoteManager) getIntent().getExtras().getSerializable(
+				"voteManager");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Restaurant restaurant = voteManager.getVoteResult();
+		ImageView image = (ImageView) findViewById(R.id.restaurant_image);
+		String imageUrl = restaurant.getImageUrl();
+		try {
+			URL url = new URL(imageUrl);
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			InputStream is = connection.getInputStream();
+			Bitmap bm = BitmapFactory.decodeStream(is);
+			image.setImageBitmap(bm);
+		} catch (Exception e) {
+		}
+		TextView name = (TextView) findViewById(R.id.restaurant_name);
+		name.setText(restaurant.getName());
 	}
 }
