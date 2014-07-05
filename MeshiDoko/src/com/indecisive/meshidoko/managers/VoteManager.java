b869aef30a29ef198bd532c1c166f9a3c79c883f
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-07-05 15:34:35 +0900

    Add VoteManager and VoteManagerTest

---
+++ MeshiDoko/src/com/indecisive/meshidoko/managers/VoteManager.java
@@ -0,0 +1,19 @@
+package com.indecisive.meshidoko.managers;
+
+public class VoteManager {
+	private int[] voteTotals;
+	private int voteNum;
+
+	public VoteManager(int peopleNum) {
+		voteNum = peopleNum;
+	}
+
+	// restaurantId is 0 ~ 2 for now because there are only 3 restaurants
+	public void vote(int restaurantId) {
+		voteTotals[restaurantId]++;
+	}
+	
+	public int getVoteTotalOfRestaurant(int restaurantId) {
+		return voteTotals[restaurantId];
+	}
+}
