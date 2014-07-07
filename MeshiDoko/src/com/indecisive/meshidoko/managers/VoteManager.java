d8989e1aad7c317b81024e58aa939b93fc0942d2
Junichi Kobayashi <nnujabok@gmail.com>
2014-07-07 18:45:30 +0900

    Show result

--- MeshiDoko/src/com/indecisive/meshidoko/managers/VoteManager.java
+++ MeshiDoko/src/com/indecisive/meshidoko/managers/VoteManager.java
@@ -3,6 +3,7 @@ package com.indecisive.meshidoko.managers;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.indecisive.meshidoko.models.Restaurant;
 
@@ -45,14 +46,23 @@ public class VoteManager implements Serializable {
 	}
 
 	public Restaurant getVoteResult() {
+		Random rand = new Random(System.currentTimeMillis());
 		Restaurant result = restaurants.get(0);
 		int max = 0;
 		for (int i = 0; i < 3; i++) {
 			if (voteTotals[i] > voteTotals[max]) {
 				result = restaurants.get(i);
 				max = i;
+			} else if (voteTotals[i] == voteTotals[max]) {
+				int a = rand.nextInt(2);
+				if (a == 1) {
+					result = restaurants.get(i);
+					max = i;
+				}
 			}
 		}
+		if (voteTotals[0] == voteTotals[1] && voteTotals[1] == voteTotals[2])
+			result = restaurants.get(rand.nextInt(3));
 		return result;
 	}
 }
