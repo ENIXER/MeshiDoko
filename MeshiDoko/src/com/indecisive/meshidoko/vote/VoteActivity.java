4166a674f46a48eaa0140fed27576228a9798e23
Junichi Kobayashi <nnujabok@gmail.com>
2014-07-21 00:06:27 +0900

    Update restaurant images

--- MeshiDoko/src/com/indecisive/meshidoko/vote/VoteActivity.java
+++ MeshiDoko/src/com/indecisive/meshidoko/vote/VoteActivity.java
@@ -42,7 +42,7 @@ public class VoteActivity extends Activity {
 
 		if ("G004".equals(genreCode)) {
 			// TODO: ジャンルごとに...
-			setRamenRestaurantData();
+			setJapaneseRestaurantData();
 		} else if ("G005".equals(genreCode)) {
 			// TODO: ジャンルごとに...
 			setRamenRestaurantData();
@@ -210,9 +210,9 @@ public class VoteActivity extends Activity {
 	}
 
 	private void setRamenRestaurantData() {
-		Restaurant menzy = new Restaurant(1, "麺爺あぶら　西早稲田店", "東京都新宿区大久保2-2-13",
+		Restaurant menzy = new Restaurant(1, "麺爺あぶら 西早稲田店", "東京都新宿区大久保2-2-13",
 				"油そば", R.drawable.menzy, 5);
-		Restaurant takatora = new Restaurant(2, "麺屋武蔵鷹虎　高田馬場店",
+		Restaurant takatora = new Restaurant(2, "麺屋武蔵鷹虎 高田馬場店",
 				"東京都新宿区高田馬場2-19-7", "濃厚つけ麺", R.drawable.takatora, 12);
 		Restaurant junren = new Restaurant(3, "さっぽろ純連　東京店",
 				"東京都新宿区高田馬場3-12-8 高田馬場センタービル1F", "札幌ラーメン", R.drawable.junren,
@@ -220,5 +220,17 @@ public class VoteActivity extends Activity {
 		restaurantList.add(menzy);
 		restaurantList.add(takatora);
 		restaurantList.add(junren);
+	}
+
+	private void setJapaneseRestaurantData() {
+		Restaurant aduchi = new Restaurant(1, "つけ蕎麦 安土 高田馬場本店",
+				"東京都新宿区高田馬場4-18-9", "蕎麦", R.drawable.aduchi, 18);
+		Restaurant narikura = new Restaurant(2, "成蔵", "東京都新宿区高田馬場1丁目32-11",
+				"とんかつ", R.drawable.narikura, 10);
+		Restaurant uoichi = new Restaurant(3, "築地 魚一 高田馬場店",
+				"東京都新宿区高田馬場3丁目2-5", "海鮮料理", R.drawable.uoichi, 18);
+		restaurantList.add(aduchi);
+		restaurantList.add(narikura);
+		restaurantList.add(uoichi);
 	}
 }
